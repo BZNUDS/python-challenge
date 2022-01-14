87df0e3cf9a30617193755fa004321b852216382
Brian Zdarsky <brian.zdarsky@gmail.com>
2022-01-14 11:40:41 -0600

    WIP

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,8 @@ import os
 import csv
 total_months=0
 total_net=0
-average=0
+change_tally=0
+average_change=0.0
 greatest_increase=0
 greatest_decrease=0
 
@@ -27,19 +28,28 @@ with open(csvpath, encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     # Read the header row first (skip this step if there is no header)
-    #csv_header = next(csvreader) # next consume a row
-    #print ("csv_header= "),csv_header
-
+    csv_header = next(csvreader) # next consume a row
+    print ("csv_header= ",csv_header)
+    last_row=0
     for row in csvreader:
-        print(f"{row}")
-        #print(f"{row[1]}")
-        #total_net+=int(row[1])
-        #print("total_net="), total_net
+        total_months += 1
+        #print(f"{row}")
+        #print(f"row[1]= {row[1]}")
+        total_net+=int(row[1])
         # title.append(row[1]) # respective column in each row
         # price.append(row[4])
         # subscriber_count.append(row[5])
         # num_reviews.append(row[6])
         # course_length.append(row[7])
+        if total_months>1:
+            change_tally+=(int(row[1])-last_row)
+            print("last_row=: ",last_row)
+            print("change_tally=: ",change_tally)
+        last_row==int(row[1])
+    #print(f"total_months=",total_months) 
+    #print(f"total_net=", total_net)
+    average_change=(change_tally/total_months) 
+    #print(f"Average Change: $",average_change)
 
 # courses = zip(title, price, subscriber_count, num_reviews, course_length)
 
