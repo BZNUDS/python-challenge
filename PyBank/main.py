00c1c092c2b80512d87ac47e7fa52360e9d4ecb4
Brian Zdarsky <brian.zdarsky@gmail.com>
2022-01-14 16:15:10 -0600

    Final

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,59 +7,65 @@
 
 import os
 import csv
-total_months=0
+
+#Initialize variables
+total_data_months=0  #Counts months of actual data (starts after header is read)
 total_net=0
 change_tally=0
 average_change=0.0
 greatest_increase=0
 greatest_decrease=0
-
+greatest_increase_month = "" 
+greatest_decrease_month = "" 
 csvpath = os.path.join('Resources', 'budget_data.csv')
-print(csvpath)
 
-title = []
-price = []
-subscriber_count = []
-num_reviews = []
-course_length = []
-print("csvpath=",csvpath)
 with open(csvpath, encoding='utf-8') as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader) # next consume a row
-    print ("csv_header= ",csv_header)
+    #print ("csv_header= ",csv_header)
     last_row=0
     for row in csvreader:
-        total_months += 1
+        total_data_months += 1
         #print(f"{row}")
-        #print(f"row[1]= {row[1]}")
         total_net+=int(row[1])
-        # title.append(row[1]) # respective column in each row
-        # price.append(row[4])
-        # subscriber_count.append(row[5])
-        # num_reviews.append(row[6])
-        # course_length.append(row[7])
-        if total_months>1:
-            change_tally+=(int(row[1])-last_row)
-            print("last_row=: ",last_row)
-            print("change_tally=: ",change_tally)
-        last_row==int(row[1])
-    #print(f"total_months=",total_months) 
-    #print(f"total_net=", total_net)
-    average_change=(change_tally/total_months) 
-    #print(f"Average Change: $",average_change)
+        if total_data_months >1:  # Start calculating monthly change values beginning with 2nd row of data
+            current_change=(int(row[1])-last_row)
+            change_tally+=current_change
+            if current_change >0 :  #Determine Greatest Increase
+                if current_change>greatest_increase:
+                    greatest_increase_month=row[0]
+                    greatest_increase=current_change
+            if current_change <0:   #Determine Greatest Decrease
+                if current_change<greatest_decrease:
+                    greatest_decrease_month=row[0]
+                    greatest_decrease=current_change
+        last_row=int(row[1])
 
-# courses = zip(title, price, subscriber_count, num_reviews, course_length)
+    # Print final financial analysis on screen
+    print("")
+    print("Financial Analysis")
+    print("-----------------------------")
+    print("Total Months: "+str(total_data_months))
+    print(f"Total: $"+str(total_net))
+    average_change=(change_tally/(total_data_months-1)) 
+    print("Average Change: $"+str(round(average_change,2)))
+    print("Greatest Increase in Profits: " + str(greatest_increase_month) + " " + str(f"(${greatest_increase})"))
+    print("Greatest Decrease in Profits: " + str(greatest_decrease_month) + " " + str(f"(${greatest_decrease})"))
+    print("")
 
-# # save the output file path
-# output_file = os.path.join('analysis', "output.csv")
+# save the output.txt file path
+output_file = os.path.join('analysis', "output.txt")
 
-# # open the output file, create a header row, and then write the zipped object to the csv
-# with open(output_file, "w", encoding='utf-8', newline='') as datafile:
-#     writer = csv.writer(datafile)
-
-#     writer.writerow(["Title", "Price", "Subscriber Count", "Number of Reviews", "Course Length"])
-
-#     writer.writerows(courses)
+# open the output.txt file and then write the finicial analysis to the txt file
+with open(output_file, "w", encoding='utf-8', newline='') as datafile:
+     writer = csv.writer(datafile)
+     writer.writerow(["Financial Analysis"])
+     writer.writerow(["-----------------------------"])
+     writer.writerow(["Total Months: "+str(total_data_months)])
+     writer.writerow([f"Total: $"+str(total_net)])
+     writer.writerow(["Average Change: $"+str(round(average_change,2))])
+     writer.writerow(["Greatest Increase in Profits: " + str(greatest_increase_month) + " " + str(f"(${greatest_increase})")])
+     writer.writerow(["Greatest Decrease in Profits: " + str(greatest_decrease_month) + " " + str(f"(${greatest_decrease})")])
